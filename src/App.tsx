39f39d2444f943c4546a6f817ee03dd7f7425314
Rodrigo Dias <rodrigo749>
2021-09-30 11:42:29 -0300

    Create fuction to handle addProducts to Cart

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { useQuery } from 'react-query'
 //Components
+import Item from './Item/Item'
 import Drawer from '@material-ui/core';
 import { LinearProgress } from '@material-ui/core';
 import { Grid } from '@material-ui/core';
@@ -19,12 +20,32 @@ export type CartItemType = {
 
 
 const getProducts = async (): Promise<CartItemType[]> =>
-  await (await fetch ('https://5d6da1df777f670014036125.mockapi.io/api/v1/product')).json();
+  await (await fetch('https://5d6da1df777f670014036125.mockapi.io/api/v1/product')).json();
 
 const App = () => {
-  const { data, isLoading, error} = useQuery<CartItemType[]>('products', getProducts);
+  const { data, isLoading, error } = useQuery<CartItemType[]>('products', getProducts);
   console.log(data);
-  return ( <div className="App"> Start</div>);
+
+  const getTotalItens = () => null;
+
+  const handleAddToCart = (clickedItem: CartItemType) => null;
+
+  const handleRemoveFromCart = () => null;
+
+  if (isLoading) return <LinearProgress />
+  if (error) return <div>Something went Worng...</div>;
+
+  return (
+    <Wrapper>
+      <Grid container spacing={3}>
+        {data?.map(item => (
+          <Grid item key={item.id} xs={12} sm={4}>
+            <Item item={item} handleAddToCart={handleAddToCart} />
+          </Grid>
+        ))}
+      </Grid>
+    </Wrapper>
+  );
 };
 
 export default App;
